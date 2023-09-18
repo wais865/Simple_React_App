dcd314f770960393e784cb008a0d74ddc2e92cac
Wais-MOF <wais.alizada2003@gmail.com>
2023-09-18 16:03:23 +0430

    react saved

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+const [welcome,setWelcome] = useState("welcome Wais Alizada");
+const handleWelcome = () => {
+  setWelcome("after welcome you did it!");
+};
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>{welcome}</h1>
+      <button onClick={handleWelcome}>click me</button>
     </div>
+    
   );
 }
 
